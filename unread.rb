673f5c69bae2d9b81637c9dcd194bd343a8a95cd
boxnos <boxnos@yahoo.com>
2018-03-31 04:31:14 +0900

    Add init, list, view command

--- unread.rb
+++ unread.rb
@@ -2,13 +2,16 @@
 
 require 'date'
 require 'csv'
+require 'fileutils'
 
 $program = File.basename(__FILE__)
 
 def usage(status=0)
   puts "Useage: #{$program} command ..."
   puts
-  puts "#{$program} add title page [date]"
+  puts "#{$program} list"
+  puts "#{$program} view title"
+  puts "#{$program} add  title page [date]"
 
   exit status
 end
@@ -18,32 +21,55 @@ def error(message)
   usage 1
 end
 
-def main()
-  if ARGV.length < 1
-    usage
+def check_argv(length=1)
+  if ARGV.length < length
+    error "arguments error."
   end
+end
+
+def get_csv_filename(title)
+  "./data/#{title}.csv"
+end
+
+def read_table(title)
+  CSV.read(get_csv_filename title)
+end
+
+def add()
+  check_argv(2)
+
+  title = ARGV.shift
+  page = ARGV.shift
+  date = Date.today
+
+  # TODO can't open new book
+  table = read_table title
+
+  # TODO search same date and update page
+  table.unshift [date, page]
+
+  CSV.open(get_csv_filename(title), "w") do |csv|
+    table.each do |line|
+      csv << line
+    end
+  end
+end
+
+def main()
+  check_argv
 
   case ARGV.shift
   when "add"
-    if ARGV.length < 2
-      error "argument error [add]"
-    end
-
-    title = ARGV.shift
-    page = ARGV.shift
-    date = Date.today
-
-    # TODO can't open new book
-    csv_filename = "./data/#{title}.csv"
-    table = CSV.read(csv_filename)
-
-    # TODO search same date and update page
-    table.unshift [date, page]
-
-    CSV.open(csv_filename, "w") do |csv|
-      table.each do |line|
-        csv << line
-      end
+    add()
+  when "init"
+    check_argv
+    FileUtils.touch(get_filename)
+  when "view"
+    check_argv
+    pp CSV.read(get_csv_filename ARGV.shift)
+  when "list"
+    Dir.glob("./data/*.csv").each do |file|
+      puts File.basename(file, ".csv")
     end
   else
     error "undefined command"
